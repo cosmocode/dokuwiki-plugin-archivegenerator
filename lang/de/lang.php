147b87b50e11d76e97d7a9f621ae468e50bdd974
Andreas Gohr <gohr@cosmocode.de>
2018-10-15 14:39:10 +0200

    German translation

---
+++ lang/de/lang.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * English language file for archivegenerator plugin
+ *
+ * @author Michael Große <dokuwiki@cosmocode.de>
+ */
+
+// menu entry for admin plugins
+$lang['menu'] = 'Wiki Archiv erzeugen';
+
+
+// custom language strings for the plugin
+$lang['label: admin mail'] = 'E-Mail-Adresse des erzeugten admin Nutzers';
+$lang['label: admin pass'] = 'Passwort des erzeugten admin Nutzers';
+$lang['button: generate archive'] = 'Wiki Archiv erzeugen';
+$lang['label: download'] = 'Wiki Archiv Download';
+
+$lang['message: archive exists'] = 'Das Wiki Archiv wurde zuletzt am %s erzeugt.';
+$lang['message: password empty'] = 'Das Passwort darf nicht leer sein!';
+$lang['message: email invalid'] = 'Die eingegbene  E-Mail-Adresse "%s" ist ungültig!';
+$lang['message: starting'] = 'Starte';
+$lang['message: adding data done'] = 'Hinzufügen von Daten abgeschlossen!';
+$lang['message: done'] = 'Eledigt!';
+$lang['message: add files in dir'] = 'Füge Dateien in %s zum Archiv hinzu.';
+$lang['message: recursive'] = 'Recursiv.';
+$lang['message: skipping files'] = 'Überspringe Dateien die %s entsprechen';
+$lang['message: create zip archive'] = 'Erzeuge ZIP Archiv %s';
+$lang['message: create users'] = 'Erzeuge neue users.auth.php mit übergebenen Daten';
+$lang['message: create acl'] = 'Erzeuge neue acl.auth.php mit minimalen Berechtigungen';
+$lang['message: create empty dir'] = 'Füge leere %s hinzu.';
+$lang['message: file is large'] = 'Datei %s ist sehr groß:';
+
+
+$lang['link: download now'] = 'Jetzt herunterladen!';
+
+$lang['seconds'] = '%s Sekunden';
---
+++ lang/de/settings.php
@@ -0,0 +1,3 @@
+<?php
+
+$lang['pluginsToIgnore'] = 'Plugins ignorieren (kommasepariert)';
